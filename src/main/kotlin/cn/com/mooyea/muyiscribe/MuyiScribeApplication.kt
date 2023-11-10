6f6dec7f73082cf3ba697cd65567fd21412976a7
lideyang <lidy@skyvis.com.cn>
2023-11-10 10:45:22 +0800

    Initial commit

---
+++ src/main/kotlin/cn/com/mooyea/muyiscribe/MuyiScribeApplication.kt
@@ -0,0 +1,11 @@
+package cn.com.mooyea.muyiscribe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MuyiScribeApplication
+
+fun main(args: Array<String>) {
+    runApplication<MuyiScribeApplication>(*args)
+}
---
+++ src/test/kotlin/cn/com/mooyea/muyiscribe/MuyiScribeApplicationTests.kt
@@ -0,0 +1,13 @@
+package cn.com.mooyea.muyiscribe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MuyiScribeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
